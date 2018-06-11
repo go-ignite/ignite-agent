00e5e6fd5989736be1f2a268e0d34b3d73c29e64
Wendell Sun <iwendellsun@gmail.com>
2018-06-11 22:25:04 +0800

    Update agent
     add CreateContainer, StartContainer and RemoveContainer functions

--- agent.go
+++ agent.go
@@ -6,13 +6,15 @@ import (
 	"io"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
 const (
-	agentLableKey   = "org.label-schema.url"
-	agentLableValue = "https://github.com/go-ignite"
+	agentLabelKey   = "org.label-schema.url"
+	agentLabelValue = "https://github.com/go-ignite"
 )
 
 func GetDockerClient() (*client.Client, error) {
@@ -34,7 +36,7 @@ func ListImages() ([]types.ImageSummary, error) {
 		return nil, fmt.Errorf("GetDockerClient error: %v", err)
 	}
 	filters := filters.NewArgs()
-	filters.Add("label", fmt.Sprintf("%s=%s", agentLableKey, agentLableValue))
+	filters.Add("label", fmt.Sprintf("%s=%s", agentLabelKey, agentLabelValue))
 	filters.Add("dangling", "false")
 	images, err := cli.ImageList(context.Background(), types.ImageListOptions{
 		All:     false,
@@ -44,4 +46,48 @@ func ListImages() ([]types.ImageSummary, error) {
 		return nil, fmt.Errorf("cli.ImageList error: %v", err)
 	}
 	return images, nil
+}
+
+func CreateContainer(image, name, method, password string, port int32) (string, error) {
+	cli, err := GetDockerClient()
+	if err != nil {
+		return "", fmt.Errorf("GetDockerClient error: %v", err)
+	}
+	config := &container.Config{
+		Image: image,
+		ExposedPorts: nat.PortSet{
+			"3389/tcp": struct{}{},
+		},
+		Cmd: []string{"-k", password, "-m", method},
+	}
+	hostConfig := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"3389/tcp": []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: fmt.Sprintf("%d", port),
+				},
+			},
+		},
+		RestartPolicy: container.RestartPolicy{Name: "always"},
+	}
+	result, err := cli.ContainerCreate(context.Background(), config, hostConfig, nil, name)
+
+	return result.ID, err
+}
+
+func StartContainer(id string) error {
+	cli, err := GetDockerClient()
+	if err != nil {
+		return fmt.Errorf("GetDockerClient error: %v", err)
+	}
+	return cli.ContainerStart(context.Background(), id, types.ContainerStartOptions{})
+}
+
+func RemoveContainer(id string) error {
+	cli, err := GetDockerClient()
+	if err != nil {
+		return fmt.Errorf("GetDockerClient error: %v", err)
+	}
+	return cli.ContainerRemove(context.Background(), id, types.ContainerRemoveOptions{Force: true})
 }
